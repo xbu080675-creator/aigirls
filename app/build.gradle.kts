89fa888a2169371425233ca8209161dce43004e0
xbu080675-creator <xbu080675-creator@users.noreply.github.com>
2026-09-05 05:08:34 +0000

    v1.4.1: 版本号更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.aigirl.floatball"
         minSdk = 24
         targetSdk = 34
-        versionCode = 8
-        versionName = "1.4.0"
+        versionCode = 9
+        versionName = "1.4.1"
     }
 
     buildTypes {
